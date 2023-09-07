c66ff54cb7441104adde8c068259cc8d222e339b
EdgarEduardoRodriguez <144301675+EdgarEduardoRodriguez@users.noreply.github.com>
2023-09-07 16:40:52 -0600

    Add files via upload

---
+++ numero duplicado examen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<< "Ingrese un numero: ";
+    int num{};
+    cin>>num;
+    cout<< num<< " duplicado es: "<<num * 2 <<'\n';
+
+    return 0;
+}
