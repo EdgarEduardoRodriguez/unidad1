ddf7eb0d8de4c2aaeba556947d499b74b495a8ce
EdgarEduardoRodriguez <144301675+EdgarEduardoRodriguez@users.noreply.github.com>
2023-09-14 14:25:51 -0600

    unidad 1
    nothing

---
+++ viñeta 47.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hola" << " Mundo";
+    /*
+    @Autor : Edgar Rodriguez Briseño
+    @Fecha : 14/09/2023
+    */ 
+    return 0;
+}
---
+++ viñeta 63.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    //define una variable entera llamada x
+    int x; //esta variable no está inicializada porque no le hemos dado un valor 
+
+    //imprime el valor de una x en la pantalla
+    std::cout <<x;
+    
+    return 0;
+    /*
+    @Autor : Edgar Rodriguez Briseño
+    @Fecha : 14/09/2023
+    */ 
+}
